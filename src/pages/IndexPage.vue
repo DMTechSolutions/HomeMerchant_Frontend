fcc5226e61d064378530544a7b0e7283a550cc65
Ramu Bhukya <ramubhukya@lavar.local>
2022-06-11 20:29:54 +0530

    loading images

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,29 +1,78 @@
 <template>
-  <q-page style="margin-top: -60px">
-    <section class="about q-mb-xl q-py-xl"></section>
+  <q-page style="margin-top: 10px">
+   <div class="row justitv-center">
+    <div class="stack-item" v-for="(image, i) in images" :key="i">
+       <img class="img" :src="image.urls.small" :alt="image.alt_description" />
+      </div>
+    </div>
   </q-page>
 </template>
 
-<script setup>
-import { onMounted } from "vue";
+<script>
+import axios from "axios";
 
-const features = [
-  {
-    icon: "fas fa-check",
-    label: "Feature 1",
+export default {
+  name: "HomePage",
+  props: {
+    page: String
   },
-  {
-    icon: "fas fa-check",
-    label: "Feature 2",
+  data() {
+    return {
+      images: [],
+      pageCount: 1,
+      isLoaded: false,
+      columnNum: 0
+    };
   },
-  {
-    icon: "fas fa-check",
-    label: "Feature 3",
+  methods: {
+    getImages(topic) {
+      axios
+        .get(
+          `https://api.unsplash.com/search/photos?query=${topic}&per_page=30&page=${this.pageCount}`,
+          {
+            headers: {
+              Authorization:
+                "Client-ID cq6La0BAF3iV8t4LisXY1n8XORHw2nIo6Oe1CjLZiqM",
+              "Accept-Version": "v1"
+            }
+          }
+        )
+        .then(response => {
+          // this.images = response.data.results;
+          this.images = this.images.concat(response.data.results);
+          this.isLoaded = true;
+        })
+        .catch(() => {
+          this.images = [];
+          this.isLoaded = false;
+        });
+    }
   },
-];
-onMounted(() => {
-  console.log("Mounted.....");
-});
+  created() {
+    this.getImages(this.page);
+    this.columnNum = Math.floor(window.innerWidth / 230);
+  },
+  mounted() {
+    window.onscroll = () => {
+      let bottomOfWindow =
+        window.pageYOffset + window.innerHeight - document.body.offsetHeight;
+
+      if (bottomOfWindow > -30 && this.isLoaded) {
+        this.pageCount++;
+        this.isLoaded = false;
+        this.getImages(this.page);
+      }
+    };
+  }
+};
 </script>
 
-<style scoped></style>
+<style scoped>
+.stack-item {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  max-width: fit-content;
+
+}
+</style>
